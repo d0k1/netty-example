c2525c98a56f19223e19b8be18660f4f8b632823
doki <d0k1@focus-it.ru>
2014-12-28 18:50:38 +0500

    It works!

--- client/src/main/java/com/focusit/netty/client/App.java
+++ client/src/main/java/com/focusit/netty/client/App.java
@@ -1,10 +1,115 @@
 package com.focusit.netty.client;
 
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.PooledByteBufAllocator;
+import io.netty.buffer.WrappedByteBuf;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.net.SocketAddress;
+
 /**
  * Created by Denis V. Kirpichenkov on 28.12.14.
  */
 public class App {
-	public static void main(String[] args) {
+	static EventLoopGroup workerGroup = new NioEventLoopGroup();
+	static ChannelFuture f;
+	static Channel channel;
+	static ChannelFuture lastWrite = null;
 
+	public static void main(String[] args) throws InterruptedException {
+		Bootstrap b = new Bootstrap(); // (1)
+		b.group(workerGroup); // (2)
+		b.channel(NioSocketChannel.class); // (3)
+		b.option(ChannelOption.SO_KEEPALIVE, true); // (4)
+		b.option(ChannelOption.TCP_NODELAY, true);
+		b.handler(new ChannelInitializer<SocketChannel>() {
+			@Override
+			public void initChannel(SocketChannel ch) throws Exception {
+					ch.pipeline().addLast(getHandler());
+			}
+		});
+
+		// Start the client.
+		f = b.connect("127.0.0.1", 16000).sync(); // (5)
+		channel = f.await().channel();
+		Thread beat = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while(!Thread.interrupted()){
+					for(int i=0;i<10;i++) {
+						try {
+							if (channel.isWritable()){
+								ByteBuf buffer = PooledByteBufAllocator.DEFAULT.buffer();
+								buffer.writeBytes("123".getBytes());
+								lastWrite = channel.write(buffer);
+							}
+							Thread.sleep(200);
+
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+					}
+					channel.flush();
+//					if(lastWrite!=null) {
+//						try {
+//							lastWrite.sync();
+//							lastWrite = null;
+//						} catch (InterruptedException e) {
+//							e.printStackTrace();
+//						}
+//					}
+				}
+			}
+		});
+		beat.start();
+	}
+
+	public static ChannelHandler getHandler(){
+		return new ChannelHandlerAdapter(){
+			@Override
+			public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+				System.err.println("error: "+cause.toString());
+			}
+
+			@Override
+			public void connect(ChannelHandlerContext ctx, SocketAddress remoteAddress, SocketAddress localAddress, ChannelPromise promise) throws Exception {
+				System.err.println("Connect");
+				super.connect(ctx, remoteAddress, localAddress, promise);
+			}
+
+			@Override
+			public void disconnect(ChannelHandlerContext ctx, ChannelPromise promise) throws Exception {
+				System.err.println("Disconnect");
+				super.disconnect(ctx, promise);
+			}
+
+			@Override
+			public void close(ChannelHandlerContext ctx, ChannelPromise promise) throws Exception {
+				System.err.println("Close");
+				super.close(ctx, promise);
+			}
+
+			@Override
+			public void read(ChannelHandlerContext ctx) throws Exception {
+				System.err.println("Read");
+				super.read(ctx);
+			}
+
+			@Override
+			public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+				System.err.println("Write");
+				super.write(ctx, msg, promise);
+			}
+
+			@Override
+			public void flush(ChannelHandlerContext ctx) throws Exception {
+				System.err.println("Flush");
+				super.flush(ctx);
+			}
+		};
 	}
 }
--- server/src/main/java/com/focusit/netty/server/App.java
+++ server/src/main/java/com/focusit/netty/server/App.java
@@ -1,10 +1,85 @@
 package com.focusit.netty.server;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.ReplayingDecoder;
+import io.netty.util.ReferenceCountUtil;
+
+import java.util.List;
+
 /**
  * Created by Denis V. Kirpichenkov on 28.12.14.
  */
 public class App {
-	public static void main(String[] args) {
+	private static ChannelFuture f;
 
+
+	public static void main(String[] args) {
+		EventLoopGroup bossGroup = new NioEventLoopGroup(); // (1)
+		EventLoopGroup workerGroup = new NioEventLoopGroup();
+		try {
+			ServerBootstrap b = new ServerBootstrap(); // (2)
+			b.group(bossGroup, workerGroup)
+				.channel(NioServerSocketChannel.class) // (3)
+				.childHandler(new ChannelInitializer<SocketChannel>() { // (4)
+					@Override
+					public void initChannel(SocketChannel ch) throws Exception {
+						ch.pipeline().addLast(getDecoder(), getHandler());
+					}
+				})
+				.option(ChannelOption.SO_BACKLOG, 128)          // (5)
+				.childOption(ChannelOption.SO_KEEPALIVE, true); // (6)
+
+			// Bind and start to accept incoming connections.
+			f = b.bind(16000).sync(); // (7)
+
+			// Wait until the server socket is closed.
+			// In this example, this does not happen, but you can do that to gracefully
+			// shut down your server.
+			f.channel().closeFuture().sync();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		workerGroup.shutdownGracefully();
+		bossGroup.shutdownGracefully();
+	}
+
+	public static ReplayingDecoder getDecoder(){
+		return new ReplayingDecoder<Void>(){
+
+			@Override
+			protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
+				int count = in.readableBytes();
+				if(count>3){
+
+					byte msg[] = new byte[3];
+					in.readBytes(msg);
+					out.add(new String(msg));
+				}
+			}
+		};
+	}
+
+	public static ChannelHandler getHandler(){
+		return new ChannelHandlerAdapter() { // (1)
+
+			@Override
+			public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
+				System.err.println(msg.getClass().getName());
+				// Discard the received data silently.
+				ReferenceCountUtil.release(msg);
+			}
+
+			@Override
+			public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
+				// Close the connection when an exception is raised.
+				cause.printStackTrace();
+				ctx.close();
+			}
+		};
 	}
 }
